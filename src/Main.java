1f7f48b218564eba67696aac3bbd759078e39772
sandraszu <sandra.szu@iths.se>
2024-10-19 22:33:38 +0200

    debug: getting warning after using TreeMap for two menu Samples MenuHub.mainMenu and MenuHub.createRecipeMenu

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import utility.MenuHub;
+
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("RecipeGenie WIP still...");
+        System.out.println(MenuHub.mainMenu());
     }
 }
--- src/utility/MenuHub.java
+++ src/utility/MenuHub.java
@@ -1,11 +1,31 @@
 package utility;
 
+import java.util.TreeMap;
+
 public class MenuHub {
 
-    String temporary = "This is a placeholder for menu choices";
+    private static final TreeMap<Integer, String> mainMenu = new TreeMap<>();
+    private static final TreeMap<Integer, String> createRecipeMenu = new TreeMap<>();
+    //private static final TreeMap<Integer, String> modifyRecipeMenu = new TreeMap<>();
 
-    public void getTemporary(){
-        System.out.println(temporary);
+
+    public static TreeMap mainMenu(){
+        mainMenu.put(1, "Create Recipe.");
+        mainMenu.put(2, "View Recipe");
+        //mainMenu.put(3, "Modify Recipe");
+        mainMenu.put(3, "Delete Recipe");
+        mainMenu.put(4, "Exit");
+        return mainMenu;
+    }
+
+    public static TreeMap createRecipeMenu(){
+        createRecipeMenu.put(1, "Add ingredient");
+        createRecipeMenu.put(2, "Add instructions");
+        createRecipeMenu.put(3, "Assign category");
+        createRecipeMenu.put(4, "Preview recipe");
+        createRecipeMenu.put(5, "Save recipe");
+        createRecipeMenu.put(6, "Discard recipe");
+        return createRecipeMenu;
     }
 
 }
