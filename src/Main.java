8af1c956a4303eae26c40aa6e949ba48f53c7dc5
sandraszu <sandra.szu@iths.se>
2024-10-19 23:53:34 +0200

    fixed: resolved warning of using TreeMap with raw types instead of using Generics.
    added static blocks to initial existing menus once
    added separte methods to extract initialized menus
    method and specified Generic types for TreeMap
    
    chore: @TODO to fix toString overrides as currently the whole TreepMap
    would be returned as a single line String

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import utility.MenuHub;
 public class Main {
     public static void main(String[] args) {
         System.out.println("RecipeGenie WIP still...");
-        System.out.println(MenuHub.mainMenu());
+        System.out.println(MenuHub.getMainMenu());
+        System.out.println(MenuHub.getCreateRecipe());
     }
 }
--- src/utility/MenuHub.java
+++ src/utility/MenuHub.java
@@ -5,27 +5,40 @@ import java.util.TreeMap;
 public class MenuHub {
 
     private static final TreeMap<Integer, String> mainMenu = new TreeMap<>();
-    private static final TreeMap<Integer, String> createRecipeMenu = new TreeMap<>();
+    private static final TreeMap<Integer, String> createRecipe = new TreeMap<>();
     //private static final TreeMap<Integer, String> modifyRecipeMenu = new TreeMap<>();
 
+    static {
+        initializeMain();
+        initializeCreateRecipe();
+    }
 
-    public static TreeMap mainMenu(){
-        mainMenu.put(1, "Create Recipe.");
+
+    public static void initializeMain(){
+        mainMenu.put(1, "Create Recipe");
         mainMenu.put(2, "View Recipe");
         //mainMenu.put(3, "Modify Recipe");
         mainMenu.put(3, "Delete Recipe");
         mainMenu.put(4, "Exit");
+    }
+
+    public static void initializeCreateRecipe(){
+        createRecipe.put(1, "Add ingredient");
+        createRecipe.put(2, "Add instructions");
+        createRecipe.put(3, "Assign category");
+        createRecipe.put(4, "Preview recipe");
+        createRecipe.put(5, "Save recipe");
+        createRecipe.put(6, "Discard recipe");
+    }
+
+    public static TreeMap<Integer, String> getMainMenu(){
         return mainMenu;
     }
 
-    public static TreeMap createRecipeMenu(){
-        createRecipeMenu.put(1, "Add ingredient");
-        createRecipeMenu.put(2, "Add instructions");
-        createRecipeMenu.put(3, "Assign category");
-        createRecipeMenu.put(4, "Preview recipe");
-        createRecipeMenu.put(5, "Save recipe");
-        createRecipeMenu.put(6, "Discard recipe");
-        return createRecipeMenu;
+    public static TreeMap<Integer, String> getCreateRecipe(){
+        return createRecipe;
     }
+
+//@TODO: Fix toString Override for TreeMap
 
 }
