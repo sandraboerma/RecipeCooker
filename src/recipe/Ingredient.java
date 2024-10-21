f0d00a79c68097d7e6d7521d1fcaec9d9d19143f
sandraszu <sandra.szu@iths.se>
2024-10-21 20:03:51 +0200

    chore: added CreateRecipe class for recipe creation related operation - wip

--- src/recipe/Ingredient.java
+++ src/recipe/Ingredient.java
@@ -26,4 +26,16 @@ public class Ingredient {
     public String getUnit() {
         return unit;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 }
--- src/service/CreateRecipe.java
+++ src/service/CreateRecipe.java
@@ -6,11 +6,28 @@ import utility.ScannerManagement;
 public class CreateRecipe {
     public static void addIngredient(){
         boolean keepAddingIngredient = false;
+        Ingredient addIngredient = new Ingredient();
         do {
-            System.out.println("");
+            System.out.println("Enter the ingredient name: "); // change phrase later
+            String input = ScannerManagement.getUserInput();
+            String[] collectIngredient = input.split(",");
+            String name = collectIngredient[0].trim();
+            double quantity = Double.parseDouble(collectIngredient[1].trim());
+            String unit = collectIngredient[2].trim();
+            if (collectIngredient.length == 3) {
+                addIngredient.setName(name);
+                addIngredient.setQuantity(quantity);
+                addIngredient.setUnit(unit);
+
+                System.out.println("Would you like to add another ingredient?");
+            } else {
+                System.out.println("Wrong input. please try again");
+            }
+
+
+
+
         } while (keepAddingIngredient);
-        ScannerManagement.getUserInput();
-        Ingredient abc = new Ingredient();
     }
 
 
