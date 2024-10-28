10f7366dc29148cb8f73b12434c5a727c99b0366
sandraszu <sandra.szu@iths.se>
2024-10-28 10:33:55 +0100

    refactor(AddToRecipe): add AddToRecipe to host methods supplementting RecipeBuilder for logic to add other data categories

--- src/service/RecipeBuilder.java
+++ src/service/RecipeBuilder.java
@@ -56,41 +56,9 @@ public class RecipeBuilder {
         }
 
         if (recipe != null) {
-            addIngredientsToRecipe(recipe);
-            addInstructionsToRecipe(recipe);
+            AddToRecipe.ingredients(recipe);
+            AddToRecipe.instructions(recipe);
             System.out.println(DisplayFormatter.getFormattedRecipe(recipe));
         }
-    }
-
-    public static void addIngredientsToRecipe(Recipe recipe) {
-        boolean addingIngredient;
-
-        do {
-            String name = InputScanner.getUserInput("""
-                    Pray tell, what be the name of the ingredient?
-                    Speak its name, O wise one:\s""").trim();
-            String unit = InputScanner.getUserInput("""
-                    By what measure shall we quantify this ingredient?
-                    (dl? hg? grams? cc? ounces)?:\s""").trim();
-            double quantity = InputScanner.getValidatedDoubleInput("""
-                    Tell me, how much of this ingredient dost thou require?:\s""");
-            Ingredient newIngredient = new Ingredient(name, unit, quantity);
-            recipe.addIngredient(newIngredient);
-            addingIngredient = InputScanner.getValidatedBooleanInput("""
-                    \nWouldst thou add another ingredient to this concoction?\s""");
-
-        } while (addingIngredient);
-    }
-
-    public static void addInstructionsToRecipe(Recipe recipe) {
-        boolean addingInstruction;
-
-        do {
-            String newInstruction = InputScanner.getUserInput("Recite the cooking instructions, " +
-                    "one task at a time, as though casting a spell: ");
-            recipe.addInstruction(newInstruction);
-            addingInstruction = InputScanner.getValidatedBooleanInput("""
-                    \nShall we weave more instructions into this culinary ritual?\s""");
-        } while (addingInstruction);
     }
 }
