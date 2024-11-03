9bc36758ce5a2ce60d27d6f3bb3279bd9a5cd9b0
sandraszu <sandra.szu@iths.se>
2024-11-03 13:50:52 +0100

    test: add @TempDir for isolate file and enhance IO handling in unit tests, temporary file creation and clean up after tests"

--- test/FileOperatorsTest.java
+++ test/FileOperatorsTest.java
@@ -1,32 +1,50 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import utility.FileOperators;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FileOperatorsTest {
 
+    @TempDir
+    Path tempDir;
+
     @Test
     public void testCreatePath(){
-        Path path = Path.of("");
+        Path path = tempDir.resolve("testRecipe.txt");
         assertTrue(FileOperators.createPath(path));
+        assertTrue(Files.exists(path));
     }
 
-//    @Test
-//    public void testWriteToPath(){
-//        Path path = Path.of("");
-//        assertTrue(FileOperators.writeToPath(path));
-//    }
-
-//    @Test
-//    public void testReadPath(){
-//        Path path = Path.of("");
-//        assertTrue(FileOperators.readPath(path));
-//    }
+    @Test
+    public void testWriteToPath() throws IOException {
+        Path path = tempDir.resolve("writeTestRecipe.txt");
+        FileOperators.createPath(path);
+        String content = "Test Recipe Content";
+        FileOperators.writeToPath(path, content);
+        List<String> testLines = Files.readAllLines(path);
+        assertEquals(content, testLines.get(0), "File content should match written content.");
+    }
 
     @Test
-    public void testDeletePath(){
-        Path path = Path.of("");
+    public void testReadPath() throws IOException {
+        Path path = tempDir.resolve("readTestRecipe.txt");
+        String content = "Test Recipe Content";
+        Files.write(path, List.of(content));
+        List<String> testLines = FileOperators.readPath(path);
+        assertEquals(content, testLines.get(0), "File content should match expected content");
+    }
+
+    @Test
+    public void testDeletePath() throws IOException {
+        Path path = tempDir.resolve("deleteTestRecipe.txt");
+        Files.createFile(path);
         assertTrue(FileOperators.deletePath(path));
+        assertFalse(Files.exists(path));
     }
 }
